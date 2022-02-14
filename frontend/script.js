c2b4b055ac778f467728b56153be85b32735c59f
MS-Cinti <szabadicintia@gmail.com>
2022-02-14 17:36:27 +0100

    added functions and variables

---
+++ frontend/script.js
@@ -0,0 +1,122 @@
+/*console.log(a)
+console.log(c)*/
+
+
+//console.log("Hello!")
+//console.log('Hello!')
+//console.log(`Hel
+//lo!`)
+
+//console.log(typeof 2)
+//console.log(typeof NaN)
+//console.log(0/0)
+//console.log(2.14)
+
+//console.log(true)
+//console.log(false)
+
+//console.log(undefined)
+//console.log(null)
+
+//console.log(typeof undefined)
+//console.log(typeof null)
+/* 
+console.log({
+    "title": "Start coding",
+    "type": "solo",
+    "difficulty": 4.1,
+    "usefulness": 4.2,
+    "enjoyability": 3.7,
+    "materials": 3.4,
+    "tasks": [
+        {
+            "title": "Bank accounts",
+            "description": ""
+        },
+        {
+            "title": "100 doors",
+            "description": ""
+        },
+        {
+            "title": "Word filter",
+            "description": ""
+        }
+    ]
+})
+*/
+/*
+var a = "hello1"
+let b = "hello2"
+const c = "hello3"
+
+console.log(a)
+console.log(b)
+console.log(c)
+*/
+
+
+/*function scope:*/
+/*
+function d() {
+    if(1+1 === 2){
+    }
+    var a = "hello1"
+    let b = "hello2"
+    const c = "hello3" 
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+d()
+
+*/
+/* 
+let e = 1
+console.log(e)
+
+e = 2
+console.log(e)
+
+e = e*2
+console.log(e)
+
+
+function multiplyByTwo(num) {
+    console.log(num)
+    return num*2
+}
+
+let f = multiplyByTwo(1)
+console.log(f)
+console.log(multiplyByTwo(1))
+console.log(num)
+
+*/
+/* 
+let g = {
+    myMethod: function (){
+        console.log("hello");
+    }
+}
+g.myMethod()
+
+/*                                                              //ez nem fog működni, csak pl
+let window = {
+    addEventListener: function (eventName, eventFunction){
+        if (eventName === "load"){
+            eventFunction()
+        }
+    }
+}
+window.addEventListener("load", loadEvent)
+*/
+
+let h = {
+    anotherMethod: function(text){
+        return `You sent the following argument to this method: ${text}`
+    }
+}
+let i = h.anotherMethod("argument")
+console.log(i)
+console.log(h.anotherMethod("another argument"))
