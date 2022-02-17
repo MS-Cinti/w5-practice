9a7621a3b761a6161f538e49197bfeffb7636d03
MS-Cinti <szabadicintia@gmail.com>
2022-02-17 18:28:46 +0100

    added for loop

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 function loadEvent() {
 
-    let rootElement = document.getElementById("root")  //változó a rootelemekkel = beolvasom a #root elemeit, itt érem el őket
+    let rootElement = document.getElementById("root")  
 
     let card2 = function (title, year, rate){
         return `
@@ -13,47 +13,39 @@ function loadEvent() {
     }
 
     let renderAllCardElements = function(incomingMoviesArray){
-        let renderedCardList = "";
+        let renderedCardList = `<div class="cards">`;
         
         //for ciklus, ami végig megy a cardsArrayen
         for (const incomingMovie of incomingMoviesArray) {
-            //for ciklus minden lépcsőjénél hozzáadja a renderedCardListhez az adott elemet a megfelelő div cardban
-            renderedCardList +=  `
+        //for ciklus minden lépcsőjénél hozzáadja a renderedCardListhez az adott elemet a megfelelő div cardban
+            renderedCardList += card2(incomingMovie.title, incomingMovie.year, incomingMovie.rate)
+            /* renderedCardList +=  `
             <div class="card">
             <h2>${incomingMovie.title}</h2>
             <div class="time">${incomingMovie.year}</div>
             <div class="rate">${incomingMovie.rate}</div>
             </div>
-            `
+            ` */
         }
+        renderedCardList += `</div>`
+
         console.log(renderedCardList);
         //returnöli az elkészült elemekkel feltöltött renderedCardListet
         return renderedCardList;
     }
 
-    //movies.sort(function(a, b){return a.year - b.year}); // ezzel időrendi sorrendbe teszi a filmeket
-
-    let newGoodMovies = [];
+    let newGoodMovies = []; //feltöltjük a kövi for-ral
 
     for (const movieSend of movies) {      
-        /* let newerThan2000 = false;
-        if (movieSend.year > 2000){    
-            newerThan2000 = true;
-        }
-        if (newerThan2000) {
-            rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, movieSend.rate));
-        } */
-        //let floorRate = Math.floor(movieSend.rate);
 
         if (movieSend.year > 2000 && movieSend.rate >= 8){
-            newGoodMovies.push(movieSend); //csak azok, amikre igazak az állítások
-            //rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, floorRate));
+            newGoodMovies.push(movieSend);
         }
     }
 
-    
-
-    newGoodMovies.sort(function(a, b){return a.year - b.year});
-    rootElement.insertAdjacentHTML("beforeend", renderAllCardElements(newGoodMovies));
+    newGoodMovies.sort(function(a, b){return a.year - b.year}); //sorrendbe tesszük
+    rootElement.insertAdjacentHTML("beforeend", renderAllCardElements(newGoodMovies)); 
+    //meghívom a renderAllCardElementset
 }
+
 window.addEventListener("load", loadEvent);
